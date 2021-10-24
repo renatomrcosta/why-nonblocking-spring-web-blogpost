11b13a21b9bdf26d5c63c9395acbc9b807534a41
Renato Costa <renato.rezende.costa@gmail.com>
2021-10-24 21:49:46 +0200

    futures http request

--- 1.requests.sh
+++ 1.requests.sh
@@ -1,5 +1,4 @@
 #bin/bash
-sleep 5s
 for _ in {1..50} ; do
   curl -X GET --location "http://localhost:8080/greeting" \
       -H "Accept: application/json" &
--- src/main/kotlin/de/renatomrcosta/whyreactivespringweb/WhyReactiveSpringWebApplication.kt
+++ src/main/kotlin/de/renatomrcosta/whyreactivespringweb/WhyReactiveSpringWebApplication.kt
@@ -2,12 +2,17 @@ package de.renatomrcosta.whyreactivespringweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 @SpringBootApplication
 class WhyReactiveSpringWebApplication
@@ -33,7 +38,9 @@ class GreetingController(
 }
 
 @Service
-class GreetingService {
+class GreetingService(
+    private val delayService: DelayService,
+) {
     private val hardCodedGreetings = listOf(
         Greeting(id = 1, text = "Hello there!"),
         Greeting(id = 2, text = "Howdy Partner!"),
@@ -42,16 +49,19 @@ class GreetingService {
 
     fun getAll(): List<Greeting> {
         trace("Starting work to get all greetings")
-        Thread.sleep(1_000) // Simulates a really slow amount of work, 1 sec of total pause
 
-        return hardCodedGreetings.also  {
+        delayService.delay()
+
+        return hardCodedGreetings.also {
             trace("Got all greetings!")
         }
     }
 
     fun getById(id: Int): Greeting? {
         trace("Starting work to get a specific greeting")
-        Thread.sleep(1_000) // Simulates a really slow amount of work, 1 sec of total pause
+
+        delayService.delay()
+
         return hardCodedGreetings.firstOrNull { it.id == id }.also {
             trace("Got specific greeting")
         }
@@ -62,4 +72,33 @@ data class Greeting(val id: Int, val text: String)
 
 private fun trace(msg: Any) {
     println("[${Thread.currentThread().name}] $msg")
+}
+
+@Service
+class DelayService(
+    private val javaClient: java.net.http.HttpClient,
+) {
+    fun delay() {
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create("$ENDPOINT_URL/$DELAY_SECONDS"))
+            .GET()
+            .build()
+
+        val future = javaClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+
+        future.thenAccept {
+            trace("Waiting finished with status ${it.statusCode()}")
+        }
+    }
+
+    companion object {
+        private const val ENDPOINT_URL = "https://httpbin.org/delay"
+        private const val DELAY_SECONDS = 1
+    }
+}
+
+@Configuration
+class HttpClientConfiguration {
+    @Bean
+    fun javaClient(): java.net.http.HttpClient = java.net.http.HttpClient.newHttpClient()
 }
